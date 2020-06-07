6d53f47869f6f3a6c1adad77cf11f6ddeb173910
hank <896977141@qq.com>
2020-06-07 17:50:48 +0800

    数据库配置

--- src/common/config/database.js
+++ src/common/config/database.js
@@ -2,12 +2,12 @@ const mysql = require('think-model-mysql');
 
 module.exports = {
     handle: mysql,
-    database: 'hiolabsdb',
+    database: 'olm',
     prefix: 'hiolabs_',
     encoding: 'utf8mb4',
     host: '121.36.108.165',
     port: '3306',
-    user: 'prod',
-    password: 'lianconn@Prod',
+    user: 'olm',
+    password: 'olm@123A',
     dateStrings: true
 };
